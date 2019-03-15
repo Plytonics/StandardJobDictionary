e36bcee82ee53c53ce4aa11acd0462dfd17c80db
Derick Vigne <derickvigne@me.com>
2019-03-14 23:38:07 -0400

    Fixed Indentation

--- Admin_Interface.php
+++ Admin_Interface.php
@@ -7,161 +7,161 @@
 <!DOCTYPE HTML>
 <html>
 <!-- SJD_index -->
-	<head>
-		<script>
+<head>
+	<script>
 
-		$(document).ready(function()
+	$(document).ready(function()
+	{
+		$('.button').click(function()
 		{
-			$('.button').click(function()
-			{
-				var clickBtnValue = $(this).val();
-				var ajaxurl = 'Admin_Interface.php',
-				data =  {'action': clickBtnValue};
-						$.post(ajaxurl, data, function (response){
-						// Response div goes here.
-						alert("action performed successfully");
-						});
+			var clickBtnValue = $(this).val();
+			var ajaxurl = 'Admin_Interface.php',
+			data =  {'action': clickBtnValue};
+			$.post(ajaxurl, data, function (response){
+				// Response div goes here.
+				alert("action performed successfully");
 			});
 		});
+	});
 
-		</script>
-	</head>
+	</script>
+</head>
 
-    <body bgcolor = "#FFFFFF">
+<body bgcolor = "#FFFFFF">
 
 
-        <div align = "center">
-         <div style = "width:1200px; border: solid 1px #228B22; " align = "left">
-            <div style = "background-color:#228B22; color:#FFFFFF; padding:3px;"><b>Admin</b></div>
-            <div style = "margin:30px">
+	<div align = "center">
+		<div style = "width:1200px; border: solid 1px #228B22; " align = "left">
+			<div style = "background-color:#228B22; color:#FFFFFF; padding:3px;"><b>Admin</b></div>
+			<div style = "margin:30px">
 
 				<div align = "center">
-				<form action="Admin_Interface.php?go" method="post" id="searchform">
-				<input type="text" name="search" placeholder="Search for user..."/>
-				<input type="submit" name="submit" value="search"/>
+					<form action="Admin_Interface.php?go" method="post" id="searchform">
+						<input type="text" name="search" placeholder="Search for user..."/>
+						<input type="submit" name="submit" value="search"/>
+					</form>
 				</form>
-                </form>
-				</div>
+			</div>
 
-				<?php
-				if(isset($_POST['submit'])){
-	  if(isset($_GET['go'])){
-	  if(preg_match("/^[  a-zA-Z]+/", $_POST['search'])){
-	  $search=$_POST['search'];
-	  //connect  to the database
-	  $servername = "localhost";
-$username = "username";
-$password = "password";
-$dbname = "mydb";
+			<?php
+			if(isset($_POST['submit'])){
+				if(isset($_GET['go'])){
+					if(preg_match("/^[  a-zA-Z]+/", $_POST['search'])){
+						$search=$_POST['search'];
+						//connect  to the database
+						$servername = "localhost";
+						$username = "username";
+						$password = "password";
+						$dbname = "mydb";
 
-	// Create connection
-	$conn = new mysqli($servername, $username, $password, $dbname);
-	// Check connection
-	if ($conn->connect_error)
-	{
-		die("Connection failed: " . $conn->connect_error);
-	}
-	  //-query  the database table
-	  //$sql ="SELECT * FROM `userinfo` WHERE firstname LIKE '%" . $search .  "%' OR lastname LIKE '%" . $search ."%'";
-	  $sql="SELECT id, firstname, lastname FROM userinfo WHERE firstname LIKE '%" . $search .  "%' OR lastname LIKE '%" . $search ."%'";
+						// Create connection
+						$conn = new mysqli($servername, $username, $password, $dbname);
+						// Check connection
+						if ($conn->connect_error)
+						{
+							die("Connection failed: " . $conn->connect_error);
+						}
+						//-query  the database table
+						//$sql ="SELECT * FROM `userinfo` WHERE firstname LIKE '%" . $search .  "%' OR lastname LIKE '%" . $search ."%'";
+						$sql="SELECT id, firstname, lastname FROM userinfo WHERE firstname LIKE '%" . $search .  "%' OR lastname LIKE '%" . $search ."%'";
 
-	  //-run  the query against the mysql query function
-	  $result = $conn->query($sql) or die("could not search!");
+						//-run  the query against the mysql query function
+						$result = $conn->query($sql) or die("could not search!");
 
-	  //-create  while loop and loop through result set
-	  while($row=mysqli_fetch_array($result)){
-						$ID = $row['id'];
-	          $Firstname1  =$row['firstname'];
-	  //-display the result of the array
-	  echo "<ul>\n";
-	  echo "<li>" . "<a  href=\"search.php?id=$ID\">"   .$Firstname1 . "</a></li>\n";
-	  echo "</ul>";
-	  }
-	  }
-	  else{
-	  echo  "<p>Please enter a search query</p>";
-	  }
-	  }
-	  }
-				?>
+						//-create  while loop and loop through result set
+						while($row=mysqli_fetch_array($result)){
+							$ID = $row['id'];
+							$Firstname1  =$row['firstname'];
+							//-display the result of the array
+							echo "<ul>\n";
+							echo "<li>" . "<a  href=\"search.php?id=$ID\">"   .$Firstname1 . "</a></li>\n";
+							echo "</ul>";
+						}
+					}
+					else{
+						echo  "<p>Please enter a search query</p>";
+					}
+				}
+			}
+			?>
 			<p>
 			</p>
 
 			<div align = "center">
-		    <div style = "width:600px; height: 500px; border: solid 1px #000000; " align = "left">
-            <div style = "background-color:#228B22; color:#FFFFFF; padding:0px;"><b></b></div>
-            <div style = "margin:30px">
+				<div style = "width:600px; height: 500px; border: solid 1px #000000; " align = "left">
+					<div style = "background-color:#228B22; color:#FFFFFF; padding:0px;"><b></b></div>
+					<div style = "margin:30px">
 
 
 
 
-			</div>
-			</div>
+					</div>
+				</div>
 			</div>
 
 
 
 			<div align = "left">
-			<div style = "width:200px; border: solid 1px #000000; " align = "left">
-            <div style = "background-color:#228B22; color:#FFFFFF; padding:0px;"><b></b></div>
-            <div style = "margin:20px">
+				<div style = "width:200px; border: solid 1px #000000; " align = "left">
+					<div style = "background-color:#228B22; color:#FFFFFF; padding:0px;"><b></b></div>
+					<div style = "margin:20px">
 
 
-				<form action="SJD_get_DB.php" method="post">
-                <label>User ID:  </label><input type="text" name="userid"><br>
-                <input type="submit" name="DeleteBT" value="Delete" /><br />
-                </form>
+						<form action="SJD_get_DB.php" method="post">
+							<label>User ID:  </label><input type="text" name="userid"><br>
+							<input type="submit" name="DeleteBT" value="Delete" /><br />
+						</form>
+					</div>
+				</div>
 			</div>
-			</div>
-			</div>
 
 
 
 
 
-            <div style = "font-size:11px; color:#cc0000; margin-top:10px"></div>
+			<div style = "font-size:11px; color:#cc0000; margin-top:10px"></div>
 
 
 			<div align = "center">
-			<div id="piechart"></div>
-			<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-			<script type="text/javascript">
-			// Load google charts
-			google.charts.load('current', {'packages':['corechart']});
-			google.charts.setOnLoadCallback(drawChart);
+				<div id="piechart"></div>
+				<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+				<script type="text/javascript">
+				// Load google charts
+				google.charts.load('current', {'packages':['corechart']});
+				google.charts.setOnLoadCallback(drawChart);
 
-			// Draw the chart and set the chart values
-			function drawChart() {
-			  var data = google.visualization.arrayToDataTable([
-			  ['Task', 'Hours per Day'],
-			  ['Engineering', 8],
-			  ['Health', 2],
-			  ['Manufacturing', 4],
-			  ['Finance', 2],
-			  ['Energy', 8]
-			]);
+				// Draw the chart and set the chart values
+				function drawChart() {
+					var data = google.visualization.arrayToDataTable([
+						['Task', 'Hours per Day'],
+						['Engineering', 8],
+						['Health', 2],
+						['Manufacturing', 4],
+						['Finance', 2],
+						['Energy', 8]
+					]);
 
-			  // Optional; add a title and set the width and height of the chart
-			  var options = {'Title':'Job listings', 'width':550, 'height':400};
+					// Optional; add a title and set the width and height of the chart
+					var options = {'Title':'Job listings', 'width':550, 'height':400};
 
-			  // Display the chart inside the <div> element with id="piechart"
-			  var chart = new google.visualization.PieChart(document.getElementById('piechart'));
-			  chart.draw(data, options);
-			}
-			</script>
+					// Display the chart inside the <div> element with id="piechart"
+					var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+					chart.draw(data, options);
+				}
+				</script>
 			</div>
 
 
 
 			<form action="MainPage.html">
-            <input type="submit" value="Back" />
-		    </form>
-			</div>
-            </div>
-         </div>
-      </div>
+				<input type="submit" value="Back" />
+			</form>
+		</div>
+	</div>
+</div>
+</div>
 
 
 
-   </body>
+</body>
 </html>
--- SJD_get_employer.php
+++ SJD_get_employer.php
@@ -5,55 +5,55 @@ $password = "password";
 $dbname = "jobdb";
 
 // Create connection
-	$conn = new mysqli($servername, $username, $password, $dbname);
-	// Check connection
-	if ($conn->connect_error)
-	{
-		die("Connection failed: " . $conn->connect_error);
-	}
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error)
+{
+	die("Connection failed: " . $conn->connect_error);
+}
 
 /*******once you create the database you dont need these commands anymore*********/
 //Create database
 /*$sql = "CREATE DATABASE Jobdb";
 if ($conn->query($sql) === TRUE) {
-    echo "Database created successfully";
+echo "Database created successfully";
 } else {
-    echo "Error creating database: " . $conn->error;
+echo "Error creating database: " . $conn->error;
 }*/
 $sql = "CREATE TABLE jobInfo (
-id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-title VARCHAR(30),
-degree VARCHAR(30),
-transport VARCHAR(3),
-remote VARCHAR(3),
-addr VARCHAR(30),
-stat VARCHAR(30),
-type VARCHAR(30),
-salary INT(6),
-task VARCHAR(30),
-skillreq VARCHAR(3),
-skillreqpro INT(10),
-langreq VARCHAR(30),
-langreqpro INT(10),
-summary VARCHAR(100)
+	id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	title VARCHAR(30),
+	degree VARCHAR(30),
+	transport VARCHAR(3),
+	remote VARCHAR(3),
+	addr VARCHAR(30),
+	stat VARCHAR(30),
+	type VARCHAR(30),
+	salary INT(6),
+	task VARCHAR(30),
+	skillreq VARCHAR(3),
+	skillreqpro INT(10),
+	langreq VARCHAR(30),
+	langreqpro INT(10),
+	summary VARCHAR(100)
 )";
 
 $sql = "CREATE TABLE ArcInfo (
-id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-title VARCHAR(30),
-degree VARCHAR(30),
-transport VARCHAR(3),
-remote VARCHAR(3),
-addr VARCHAR(30),
-stat VARCHAR(30),
-type VARCHAR(30),
-salary INT(6),
-task VARCHAR(30),
-skillreq VARCHAR(3),
-skillreqpro INT(10),
-langreq VARCHAR(30),
-langreqpro INT(10),
-summary VARCHAR(100)
+	id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	title VARCHAR(30),
+	degree VARCHAR(30),
+	transport VARCHAR(3),
+	remote VARCHAR(3),
+	addr VARCHAR(30),
+	stat VARCHAR(30),
+	type VARCHAR(30),
+	salary INT(6),
+	task VARCHAR(30),
+	skillreq VARCHAR(3),
+	skillreqpro INT(10),
+	langreq VARCHAR(30),
+	langreqpro INT(10),
+	summary VARCHAR(100)
 )";
 
 
@@ -140,109 +140,109 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
 		}
 
 		$sql = "INSERT INTO jobinfo (id, title, degree, transport, remote, addr, stat, type,
-				salary, task, skillreq, skillreqpro, langreq, langreqpro, summary)
-		VALUES ('0','$insert_jobtitle', '$insert_degree', '$insert_transport', '$insert_remote', '$insert_workaddr',
+			salary, task, skillreq, skillreqpro, langreq, langreqpro, summary)
+			VALUES ('0','$insert_jobtitle', '$insert_degree', '$insert_transport', '$insert_remote', '$insert_workaddr',
 				'$insert_status','$insert_jobtype','$insert_salary','$insert_task','$insert_skillreq','$insert_skillreqpro',
 				'$insert_langreq','$insert_langreqpro','$insert_summary')";
-	}
+			}
 
-    if (isset($_POST['ArcivBT']))
-	{
-		$insert_jobtitle = htmlspecialchars($_REQUEST['jobtitle']);
-		if (empty($insert_jobtitle))
-		{
-			echo "jobtitle is empty";
-		}
-		$insert_degree = htmlspecialchars($_REQUEST['degree']);
-		if (empty($insert_degree))
-		{
-			echo "degree is empty";
-		}
-		$insert_transport = htmlspecialchars($_REQUEST['transport']);
-		if (empty($insert_transport))
-		{
-			echo "transport is empty";
-		}
-		$insert_remote = htmlspecialchars($_REQUEST['remote']);
-		if (empty($insert_remote))
-		{
-			echo "remote is empty";
-		}
-		$insert_workaddr = htmlspecialchars($_REQUEST['workaddress']);
-		if (empty($insert_workaddr))
-		{
-			echo "workaddress is empty";
-		}
-		$insert_status = htmlspecialchars($_REQUEST['preferstatus']);
-		if (empty($insert_status))
-		{
-			echo "preferstatus is empty";
-		}
-		$insert_jobtype = htmlspecialchars($_REQUEST['jobtype']);
-		if (empty($insert_jobtype))
-		{
-			echo "jobtype is empty";
-		}
-		$insert_salary = htmlspecialchars($_REQUEST['salary']);
-		if (empty($insert_salary))
-		{
-			echo "salary is empty";
-		}
-		$insert_task = htmlspecialchars($_REQUEST['task']);
-		if (empty($insert_task))
-		{
-			echo "task is empty";
-		}
-		$insert_skillreq = htmlspecialchars($_REQUEST['skillreq']);
-		if (empty($insert_skillreq))
-		{
-			echo "skillreq is empty";
-		}
-		$insert_skillreqpro = htmlspecialchars($_REQUEST['skillreqpro']);
-		if (empty($insert_skillreqpro))
-		{
-			echo "skillreqpro is empty";
-		}
-		$insert_langreq = htmlspecialchars($_REQUEST['langreq']);
-		if (empty($insert_langreq))
-		{
-			echo "langreq is empty";
-		}
-		$insert_langreqpro = htmlspecialchars($_REQUEST['langreqpro']);
-		if (empty($insert_langreqpro))
-		{
-			echo "langreqpro is empty";
-		}
-		$insert_summary = htmlspecialchars($_REQUEST['summary']);
-		if (empty($insert_summary))
-		{
-			echo "summary is empty";
-		}
+			if (isset($_POST['ArcivBT']))
+			{
+				$insert_jobtitle = htmlspecialchars($_REQUEST['jobtitle']);
+				if (empty($insert_jobtitle))
+				{
+					echo "jobtitle is empty";
+				}
+				$insert_degree = htmlspecialchars($_REQUEST['degree']);
+				if (empty($insert_degree))
+				{
+					echo "degree is empty";
+				}
+				$insert_transport = htmlspecialchars($_REQUEST['transport']);
+				if (empty($insert_transport))
+				{
+					echo "transport is empty";
+				}
+				$insert_remote = htmlspecialchars($_REQUEST['remote']);
+				if (empty($insert_remote))
+				{
+					echo "remote is empty";
+				}
+				$insert_workaddr = htmlspecialchars($_REQUEST['workaddress']);
+				if (empty($insert_workaddr))
+				{
+					echo "workaddress is empty";
+				}
+				$insert_status = htmlspecialchars($_REQUEST['preferstatus']);
+				if (empty($insert_status))
+				{
+					echo "preferstatus is empty";
+				}
+				$insert_jobtype = htmlspecialchars($_REQUEST['jobtype']);
+				if (empty($insert_jobtype))
+				{
+					echo "jobtype is empty";
+				}
+				$insert_salary = htmlspecialchars($_REQUEST['salary']);
+				if (empty($insert_salary))
+				{
+					echo "salary is empty";
+				}
+				$insert_task = htmlspecialchars($_REQUEST['task']);
+				if (empty($insert_task))
+				{
+					echo "task is empty";
+				}
+				$insert_skillreq = htmlspecialchars($_REQUEST['skillreq']);
+				if (empty($insert_skillreq))
+				{
+					echo "skillreq is empty";
+				}
+				$insert_skillreqpro = htmlspecialchars($_REQUEST['skillreqpro']);
+				if (empty($insert_skillreqpro))
+				{
+					echo "skillreqpro is empty";
+				}
+				$insert_langreq = htmlspecialchars($_REQUEST['langreq']);
+				if (empty($insert_langreq))
+				{
+					echo "langreq is empty";
+				}
+				$insert_langreqpro = htmlspecialchars($_REQUEST['langreqpro']);
+				if (empty($insert_langreqpro))
+				{
+					echo "langreqpro is empty";
+				}
+				$insert_summary = htmlspecialchars($_REQUEST['summary']);
+				if (empty($insert_summary))
+				{
+					echo "summary is empty";
+				}
 
-		$sql = "INSERT INTO Arcinfo (id, title, degree, transport, remote, addr, stat, type,
-				salary, task, skillreq, skillreqpro, langreq, langreqpro, summary)
-		VALUES ('0','$insert_jobtitle', '$insert_degree', '$insert_transport', '$insert_remote', '$insert_workaddr',
-				'$insert_status','$insert_jobtype','$insert_salary','$insert_task','$insert_skillreq','$insert_skillreqpro',
-				'$insert_langreq','$insert_langreqpro','$insert_summary')";
-	}
-if (mysqli_query($conn, $sql)) {
-		echo "New record created successfully";
-	} else {
-		echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-	}
-
-
-$conn->close();
-}
-	elseif ( isset($_POST['removeBT']) )   {
-
-    //change every letter to a different colour
-	echo "this will delete the user.";
-  } else   {
-    //nothing to do here, just nice to have a comment letting you know
-  }
+				$sql = "INSERT INTO Arcinfo (id, title, degree, transport, remote, addr, stat, type,
+					salary, task, skillreq, skillreqpro, langreq, langreqpro, summary)
+					VALUES ('0','$insert_jobtitle', '$insert_degree', '$insert_transport', '$insert_remote', '$insert_workaddr',
+						'$insert_status','$insert_jobtype','$insert_salary','$insert_task','$insert_skillreq','$insert_skillreqpro',
+						'$insert_langreq','$insert_langreqpro','$insert_summary')";
+					}
+					if (mysqli_query($conn, $sql)) {
+						echo "New record created successfully";
+					} else {
+						echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+					}
 
 
+					$conn->close();
+				}
+				elseif ( isset($_POST['removeBT']) )   {
+
+					//change every letter to a different colour
+					echo "this will delete the user.";
+				} else   {
+					//nothing to do here, just nice to have a comment letting you know
+				}
 
 
-?>
+
+
+				?>

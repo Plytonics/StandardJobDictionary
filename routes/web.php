07167a6f982f24c835e91f0635c1ebeed3d12bb5
Josh <madjosh7@gmail.com>
2019-04-14 16:52:21 -0400

    routes

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,42 @@
 |
 */
 
-Route::get('/', function () {
+// indexChange
+Route::get('/StandardJobDictionary/resources/views', function () {
+    return view('indexChange');
+});
+
+// MainPage
+Route::get('/StandardJobDictionary/resources/views', function () {
+    return view('MainPage');
+});
+
+// SDJ Delete cand imdex
+Route::get('/StandardJobDictionary/resources/views', function () {
+    return view('SJD_delete_cand_index');
+});
+
+// SJD delete comp index
+Route::get('/StandardJobDictionary/resources/views', function () {
+    return view('SJD_delete_comp_index');
+});
+
+// SJD employer index
+Route::get('/StandardJobDictionary/resources/views', function () {
+    return view('SJD_employer_index');
+});
+
+// SJD get Dash
+Route::get('/StandardJobDictionary/resources/views', function () {
+    return view('SJD_index');
+});
+
+// SJD index
+Route::get('/StandardJobDictionary/resources/views', function () {
+    return view('SJD_Question');
+});
+
+// SJD Questions
+Route::get('/StandardJobDictionary/resources/views', function () {
     return view('welcome');
 });

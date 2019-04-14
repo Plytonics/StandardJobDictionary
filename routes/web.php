a834dbce0d0742868aaef6675edff081a29a0905
scr1ptkitty <eharrisgames@gmail.com>
2019-04-14 21:47:15 +0000

    add auth

--- routes/web.php
+++ routes/web.php
@@ -50,3 +50,7 @@ Route::get('SJD_Question', function () {
 Route::get('welcome', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
